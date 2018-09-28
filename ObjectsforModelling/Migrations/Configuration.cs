eb8aaabe7d343ea42a6fede4a27664e274932d9b
HarshVPatel <37636885+HarshVPatel@users.noreply.github.com>
2018-09-28 11:30:19 -0500

    making some changes

--- ObjectsforModelling/Migrations/Configuration.cs
+++ ObjectsforModelling/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace ObjectsforModelling.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using ObjectsforModelling.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,10 +18,44 @@ namespace ObjectsforModelling.Migrations
 
         protected override void Seed(ObjectsforModelling.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            // Classes to work with users and roles (provided by Microsoft packages)
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            //Check if the roles are already created.
+            //If not, create them.
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+
+            if (!context.Roles.Any(r => r.Name == "Moderator"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Moderator" });
+            }
+
+            //Check if the admin user is already created.
+            //If not, create it.
+            ApplicationUser adminUser = null;
+            //If not, add it.
+
+
+            if (!context.Users.Any(p => p.UserName == "admin@myblogapp.com"))
+            {
+                adminUser = new ApplicationUser();
+                adminUser.UserName = "admin@myblogapp.com";
+                adminUser.Email = "admin@myblogapp.com";
+                adminUser.FirstName = "Admin";
+                adminUser.LastName = "User";
+                adminUser.DisplayName = "Admin User";
+
+                userManager.Create(adminUser, "Password-1");
+            }
+            else
+            {
+                adminUser = context.Users.Where(p => p.UserName == "admin@myblogapp.com")
+                    .FirstOrDefault();
+            }
         }
     }
 }
